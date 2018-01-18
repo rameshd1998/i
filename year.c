765734de738a3cca6e22f4df683642e9d92cc4d7
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-18 16:58:45 +0530

    year.c

--- year.c
+++ year.c
@@ -9,15 +9,15 @@ int main()
         if( year%100 == 0)
         {
                       if ( year%400 == 0)
-                printf("%d is a leap year.", year);
+                printf("%d is a leap year", year);
             else
-                printf("%d is not a leap year.", year);
+                printf("%d is not a leap year", year);
         }
         else
-            printf("%d is a leap year.", year );
+            printf("%d is a leap year", year );
     }
     else
-        printf("%d is not a leap year.", year);
+        printf("%d is not a leap year", year);
     
     return 0;
 }
